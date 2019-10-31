fe3e1637d4c02266c88c615228bad6e1bc756ac5
Ajeet Singh <ajeets110@gmail.com>
2019-10-31 15:54:34 -0400

    made login work with segue

--- C0763441_MidTerm_MAD3115F2019-P1/ViewController.swift
+++ C0763441_MidTerm_MAD3115F2019-P1/ViewController.swift
@@ -10,29 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var username: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        
-        
     }
     
-    @IBOutlet weak var username: UITextField!
-    
-    @IBOutlet weak var passsword: UITextField!
     
     
+   
+   
+    
+   
     @IBAction func login(_ sender: Any) {
-        
-        let unm = username.text
-        let pass = passsword.text
-        
-        if unm == "Ajeet15" && pass == "1234"
+        let userName = username.text!
+        if userName.count > 5
         {
-            performSegue(withIdentifier: "loginSuccess", sender: nil)
-        }
-        else if unm == nil || pass == nil {
+            let pass = password.text!
+            let user = username.text!
+            if user == "Ajeet15"
+            {
+                if pass == "1234"
+                {
+                    print("Login Success")
+                    performSegue(withIdentifier: "loginSucess", sender: nil)
+                    
+                }else{
+                    print("Incorrect Password")
+                }
+            }else{
+                print("Incorrect Email")
+            }
+            
+            
+        } else{
+            let alert = UIAlertController(title: "Email Error", message: "Your Email is less than 10 charachter. Please enter a valid email", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Ignore", style: .destructive, handler: nil))
+            
+            self.present(alert, animated: true)
             
         }
     }
