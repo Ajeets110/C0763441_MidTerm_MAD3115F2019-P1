a91f5641c9f90ffe39bb6541f9b0bf27b9cc8b85
Ajeet Singh <ajeets110@gmail.com>
2019-11-01 14:31:54 -0400

    working on adding customer

--- C0763441_MidTerm_MAD3115F2019-P1/AddCustomerViewController.swift
+++ C0763441_MidTerm_MAD3115F2019-P1/AddCustomerViewController.swift
@@ -9,14 +9,22 @@
 import UIKit
 
 class AddCustomerViewController: UIViewController {
-
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var email: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func addCustomerBtn(_ sender: Any) {
+        
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
