5278ac077543bc29568605252b4e3aff0f6e4634
Ajeet <ajeets110@gmail.com>
2019-11-08 16:39:05 -0500

    created custom cell data not displaying

---
+++ C0763441_MidTerm_MAD3115F2019-P1/BillTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  BillTableViewCell.swift
+//  C0763441_MidTerm_MAD3115F2019-P1
+//
+//  Created by MacStudent on 2019-11-08.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class BillTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var billType: UITextField!
+    @IBOutlet weak var BillDate: UITextField!
+    
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var billTypelbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- C0763441_MidTerm_MAD3115F2019-P1/BillViewController.swift
+++ C0763441_MidTerm_MAD3115F2019-P1/BillViewController.swift
@@ -11,8 +11,6 @@ import UIKit
 class BillViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var tempDic:Dictionary<Int,Bill> = [:]
   
-    @IBOutlet weak var billTypeTxt: UITextField!
-    @IBOutlet weak var billType: UILabel!
     var sinObj = Singleton.getInstance()
     @IBOutlet weak var cust_detail: UILabel!
     @IBOutlet weak var cust_detail_name: UILabel!
@@ -26,6 +24,17 @@ class BillViewController: UIViewController, UITableViewDelegate, UITableViewData
             cust_detail.text = c?.customerId.intToString()
             cust_detail_name.text = c?.fullName
             cust_detail_email.text = c?.email
+            
+            if c != nil
+            {
+                billTable.dataSource = self
+                billTable.delegate = self
+                custFName.text = custTemp?.fullName
+                custEmail.text = custTemp?.customerEmail
+                if let id = custTemp?.customerID {
+                    custId.text = "\(id)"
+                }
+            }
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -39,12 +48,33 @@ class BillViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let c = sinObj.returnCustObj(custId: Int(indexPath.row+1))
-        let cell = tableView.dequeueReusableCell(withIdentifier: "BillCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BillCell", for: indexPath) as! BillTableViewCell
         //tempDic = c!.billDictionary
         //billType.text = tempDic[indexPath.row, b]
-        var billTemp = c?.returnBillArray()
-        let bTemp = billTemp?[indexPath.row].billType
-        //cell.
+        let billTemp = c?.returnBillArray()
+        let bTemp = billTemp?[indexPath.row].billType.rawValue
+        
+        if (bTemp?.elementsEqual("Mobile"))!
+        {
+            let tempObj =  billTemp![indexPath.row] as! MobileBill
+            cell.billType.text = bTemp
+            cell.BillDate.text = tempObj.billDate
+            cell.billAmount.text = String(tempObj.billAmount)
+        }
+        if (bTemp?.elementsEqual("Interner"))!
+        {
+            let tempObj =  billTemp![indexPath.row] as! InternetBill
+            cell.billType.text = bTemp
+            cell.BillDate.text = tempObj.billDate
+            cell.billAmount.text = String(tempObj.billAmount)
+        }
+        if (bTemp?.elementsEqual("Hydro"))!
+        {
+            let tempObj =  billTemp![indexPath.row] as! HydroBill
+            cell.billType.text = bTemp
+            cell.BillDate.text = tempObj.billDate
+            cell.billAmount.text = String(tempObj.billAmount)
+        }
         return cell
     }
     
