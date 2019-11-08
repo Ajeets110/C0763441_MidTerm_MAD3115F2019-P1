41907659cfefa0e84141ac9b58c6e1747d488075
Ajeet Singh <ajeets110@gmail.com>
2019-11-07 22:03:46 -0500

    added tableView in Bill controller

--- C0763441_MidTerm_MAD3115F2019-P1/BillViewController.swift
+++ C0763441_MidTerm_MAD3115F2019-P1/BillViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BillViewController: UIViewController {
+class BillViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
   
     
@@ -26,6 +26,20 @@ class BillViewController: UIViewController {
             
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sinObj.returnCount()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let c = sinObj.returnCustObj(custId: Int(indexPath.row+1))
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BillCell", for: indexPath)
+        cell.textLabel?.text = c?.fullName
+        return cell
+    }
     
 }
 
