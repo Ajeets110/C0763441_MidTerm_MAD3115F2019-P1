59a74f44d2601b2543fe11a58112a420b503fba1
Simranjeetkaur <chakkalsimmi@gmail.com>
2019-11-07 19:47:30 -0500

    added plist for login and created bill objects and passed to customer

--- C0763441_MidTerm_MAD3115F2019-P1/Singleton.swift
+++ C0763441_MidTerm_MAD3115F2019-P1/Singleton.swift
@@ -35,9 +35,19 @@ class Singleton: NSObject {
     
     //funtion to create customer objects and add using above function
     func alreadyCustomer() {
-        let c1 = Customer(customerID: 1, firstName: "Ajeet", lastName: "Singh", email: "ajeets110@gmail.com")
-        let c2 = Customer(customerID: 2, firstName: "Rakesh", lastName: "Kumar", email: "rakesh098@gmail.com")
+        // Customer 1
+        let i1 = InternetBill(providerName : "Rogers", gbUsed : 500, billAmount : 56.50, billId : 1 , billDate : "Wednesday, 19 June, 2019", billType : Bill.bTypes.Internet)
+        let m1 = MobileBill(manufacturerName:"Galaxy", planName:"Prepaid Talk", phoneNumber:6464647484, internetUsed:5, minutesUsed:356, billAmount:250.0, billId : 2, billType : Bill.bTypes.Mobile, billDate: "Wednesday, 19 June, 2019")
+        let h1 = HydroBill(billID : 3 , billDate : "Wednesday, 19 June, 2019", billType : Bill.bTypes.Hydro, agencyName : "Planet Energy", unitConsumed : 29 , billAmount : 54.35)
+        let c1 = Customer(customerID: 1, firstName: "Ajeet", lastName: "Singh", email: "ajeets110@gmail.com", billDictionary: [i1.billId : i1, m1.billId : m1, h1.billId : h1])
+        
+        // Customer 2
+        let i2 = InternetBill(providerName : "Fido", gbUsed : 600, billAmount : 70.00, billId : 4 , billDate : "Wednesday, 29 June, 2019", billType : Bill.bTypes.Internet)
+        let m2 = MobileBill(manufacturerName:"Apple", planName:"Prepaid Talk + Messages", phoneNumber:6476954211, internetUsed:6, minutesUsed:124, billAmount:80.00, billId : 5, billType : Bill.bTypes.Mobile, billDate: "Friday, 19 June, 2019")
+        let c2 = Customer(customerID: 2, firstName: "Rakesh", lastName: "Kumar", email: "rakesh098@gmail.com", billDictionary: [i2.billId : i2, m2.billId : m2])
         let c3 = Customer(customerID: 3, firstName: "Jay-", lastName: "Z", email: "jayz666@gmail.com")
+        
+        
         addCustomerToDictionary(c: c1)
         addCustomerToDictionary(c: c2)
         addCustomerToDictionary(c: c3)
@@ -45,7 +55,8 @@ class Singleton: NSObject {
     }
     
     //funtion to add customer
-    func addCustomer(firstname:String, lastname:String, email:String)  {
+    func addCustomer(firstname:String, lastname:String, email:String)
+    {
         let cId = custDict.count+1
         var c = Customer(customerID: cId, firstName: firstname, lastName: lastname, email: email)
         addCustomerToDictionary(c: c)
@@ -69,5 +80,7 @@ class Singleton: NSObject {
         }
         return nil
     }
+    
+
     
 }
--- C0763441_MidTerm_MAD3115F2019-P1/TableViewController.swift
+++ C0763441_MidTerm_MAD3115F2019-P1/TableViewController.swift
@@ -57,69 +57,13 @@ class TableViewController: UIViewController, UITableViewDelegate, UITableViewDat
         navigationController?.pushViewController(NewCustVC, animated: true)
     }
     
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-  //  }
-
-    // MARK: - Table view data source
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if #available(iOS 13.0, *) {
+            let vc = (storyboard?.instantiateViewController(identifier: "BillViewController") as? BillViewController)!
+            self.navigationController?.pushViewController(vc, animated: true)
+        } else {
+            // Fallback on earlier versions
+        }
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-
+    
 }
